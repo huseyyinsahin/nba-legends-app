c5b44fdef2453a6e162cf68b8f5e7c5b35a5cb24
huseyyinsahin <huseysahin19@outlook.com>
2024-08-26 19:43:15 +0300

    footer add

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>NBA Legends App © 2024 </p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer/Footer.jsx";
 import Header from "../components/Header/Header";
 import { data } from "../helper/data.js";
 
@@ -5,6 +6,7 @@ function HomePage() {
   return (
     <div>
       <Header data={data} />
+      <Footer/>
     </div>
   );
 }
